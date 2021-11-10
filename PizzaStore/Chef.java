2a857f37d32538af60dde702bf87d70259722aec
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-11-11 01:14:05 +0900

    Create Chef.java

---
+++ PizzaStore/Chef.java
@@ -0,0 +1,15 @@
+public class Chef {
+    
+    //피자 만드는 메소드
+    void makePizza(){
+
+        System.out.println("피자를 만드는 중입니다.");
+
+        for(int i=10; i<0; i--){
+            System.out.println(i+"초 남았습니다.");
+        }
+
+        System.out.println("피자가 완성되었습니다.");
+        
+    }
+}
