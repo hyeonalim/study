a4bfc86c433f111ca1c635a3be94aa51b7881b61
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-11-14 00:17:44 +0900

    Update Server.java

--- PizzaStore/Server.java
+++ PizzaStore/Server.java
@@ -1,9 +1,9 @@
 public class Server {
     //서빙 중
-    void serving(Customer cu){
+    void serving(Pizza pz, Customer cu){
 
         System.out.println("서빙중입니다.");
-        cu.eat();
+        cu.eat(pz);
 
     }
 }
