ed6c20491b3149b0122249cd9ce8b7a125a7fbbc
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-11-14 00:13:29 +0900

    Update Chef.java

--- PizzaStore/Chef.java
+++ PizzaStore/Chef.java
@@ -1,15 +1,21 @@
 public class Chef {
     
     //피자 만드는 메소드
-    void makePizza(){
+    //피자 안 돌려주고 있는 중.. 피자 돌려줄 것!
+    Pizza makePizza(Order od){
 
+        Pizza pz = new Pizza(); //피자 만들기
+        
         System.out.println("피자를 만드는 중입니다.");
-
-        for(int i=10; i<0; i--){
-            System.out.println(i+"초 남았습니다.");
-        }
+        
+        pz.cheese = od.amountCheese;
+        pz.olive = od.putOlive;
+        pz.beaf = od.putBeaf;
+        pz.pepperoni = od.putPepperoni;
 
         System.out.println("피자가 완성되었습니다.");
+
+        return pz;
         
     }
 }
