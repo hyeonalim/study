54ca29778d6b9951fa8248a919b95d2b31abfa88
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-11-15 23:06:25 +0900

    Update Bank.java

--- Bank/Bank.java
+++ Bank/Bank.java
@@ -17,25 +17,31 @@ public class Bank {
         Customer cu = new Customer();
 
         bker.hi();
+        
         try{
+            
+            bker.getInfo();
             bker.help(cu);
+            
         }catch(Exception e){
+            
             System.out.println(e.getMessage());
-            System.out.println("실패");
+        
         }
     }
 
-    boolean inputNext(){
+    //다른 업무 찾기
+    boolean inputWork(){
 
-        System.out.println("다음 손님을 받겠습니까?(Y/N)");
+        System.out.println("다른 업무를 확인해드릴까요? (Y/N)");
 
         Scanner scn = new Scanner(System.in); //스캐너 불러오기
-        String next = scn.nextLine();
+        String work = scn.nextLine();
         scn.nextLine();
 
-        if(next.equals("Y")){
+        if(work.equals("Y")){
             return true;
-        }else if(next.equals("N")){
+        }else if(work.equals("N")){
             return false;
         }else{
             System.out.println("Y와 N 중 입력하시지 않아 자동으로 취소됩니다.");
