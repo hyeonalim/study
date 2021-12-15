5c6dc38f405b2b9964796424ee988b93c145d166
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-12-16 01:42:50 +0900

    Create BagStewRamenOrder.java

---
+++ SnackShop/BagStewRamenOrder.java
@@ -0,0 +1,7 @@
+public class BagStewRamenOrder extends RamenOrder {
+    public BagStewRamenOrder(int num){
+        this.name = "부대찌개라면";
+        this.price = 5000;
+        this.num = num;
+    }
+}
