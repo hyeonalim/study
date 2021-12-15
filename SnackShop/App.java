1fe0e4e88e3b4ea902ac317de35a92a164a2d034
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-12-16 01:21:41 +0900

    Update App.java

--- SnackShop/App.java
+++ SnackShop/App.java
@@ -1,15 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        boolean loop = true;
+        while(true){
 
-        while(loop){
-
-            SnackShop snackShop = new SnackShop();
-            snackShop.run();
+            new Cafeteria.run();
 
         }
-
-        System.out.println("프로그램 종료");
+        
     }
 }
