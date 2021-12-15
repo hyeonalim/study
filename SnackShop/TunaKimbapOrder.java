3c3d18525091b383fd7e6c239eb5056bcc8f42af
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-12-16 01:48:53 +0900

    Create TunaKimbapOrder.java

---
+++ SnackShop/TunaKimbapOrder.java
@@ -0,0 +1,8 @@
+public class TunaKimbapOrder extends KimbapOrder {
+
+    public TunaKimbapOrder(int num){
+        this.name = "참치김밥";
+        this.price = 2000;
+        this.num = num;
+    }
+}
