b77d886be7e23f4ac871977477d8fb673a4ab273
Limhyeona <82284927+hyeonalim@users.noreply.github.com>
2021-12-16 01:22:38 +0900

    Update Chef.java

--- SnackShop/Chef.java
+++ SnackShop/Chef.java
@@ -1,35 +1,4 @@
-public class Chef {
-    
-    Maker makers(Cook cook){
-        
-        Maker maker = new Maker();
-
-        if(cook.kind.equals("김밥")){
-
-            maker = new Kimbap();
-
-        }else if(cook.kind.equals("돈까스")){
-
-            maker = new Pork();
-
-        }else if(cook.kind.equals("라면")){
-
-            maker = new Ramen();
-
-        }
-        
-        maker.kind = cook.kind;
-        maker.name = cook.name;
-        maker.num = cook.num;
-        maker.price = cook.price;
-
-        return maker;
-
-    }
-
-    void makerOff(){
-
-        System.out.println("요리가 완료되었습니다.");
-
-    }
+public abstract class Chef {
+   
+    public abstract Menu makeFood(Order o) throws Exception;
 }
